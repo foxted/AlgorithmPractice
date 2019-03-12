bca30b6917979d7bd1f710db7449a3d332c1e7de
Valentin Prugnaud <valentin@whatdafox.com>
2019-03-12 02:20:45 -0700

    Add stack

---
+++ data-structures/Stack.js
@@ -0,0 +1,37 @@
+function createStack() {
+    const stack = [];
+
+    return {
+        // push
+        push(item) {
+            stack.push(item);
+        },
+        // pop
+        pop() {
+            return stack.pop();
+        },
+        // peek
+        peek() {
+            return stack[stack.length - 1];
+        },
+        // length
+        get length() {
+            return stack.length;
+        },
+        // isEmpty
+        isEmpty() {
+            return stack.length === 0;
+        }
+    }
+}
+
+const lowerBodyStack = createStack();
+lowerBodyStack.push('Underwear');
+lowerBodyStack.push('Socks');
+lowerBodyStack.push('Pants');
+lowerBodyStack.push('Shoes');
+console.log(lowerBodyStack.peek());
+lowerBodyStack.pop();
+console.log(lowerBodyStack.peek());
+lowerBodyStack.pop();
+console.log(lowerBodyStack.length);
